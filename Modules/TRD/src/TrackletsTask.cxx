ef2de803385d3f156934b22d3a6a53c0ae6bfcd9
akavumpa <aswathy.menon@cern.ch>
2025-04-21 21:05:59 +0530

    who knows1

--- Modules/TRD/src/TrackletsTask.cxx
+++ Modules/TRD/src/TrackletsTask.cxx
@@ -189,14 +189,14 @@ void TrackletsTask::monitorData(o2::framework::ProcessingContext& ctx) // This f
 
     // Ensure histograms are initialized
     if (!mTrackletQSupermodule[supermodule][0]) {
-      mTrackletQSupermodule[supermodule][0] = std::make_unique<TH1F>(Form("Q0_Sector_%d", supermodule), Form("Q0_Sector_%d", supermodule), 100, 0, 1000);
-      mTrackletQSupermodule[supermodule][1] = std::make_unique<TH1F>(Form("Q1_Sector_%d", supermodule), Form("Q1_Sector_%d", supermodule), 100, 0, 1000);
-      mTrackletQSupermodule[supermodule][2] = std::make_unique<TH1F>(Form("Q2_Sector_%d", supermodule), Form("Q2_Sector_%d", supermodule), 100, 0, 1000);
-    
+      mTrackletQSupermodule[supermodule][0] = std::make_unique<TH1F>(Form("TrackletQ0_SM%d", supermodule), Form("TrackletQ0_SM%d", supermodule), 100, 0, 1000);
       getObjectsManager()->startPublishing(mTrackletQSupermodule[supermodule][0].get());
+
+      mTrackletQSupermodule[supermodule][1] = std::make_unique<TH1F>(Form("TrackletQ1_SM%d", supermodule), Form("TrackletQ1_SM%d", supermodule), 100, 0, 1000);
       getObjectsManager()->startPublishing(mTrackletQSupermodule[supermodule][1].get());
+
+      mTrackletQSupermodule[supermodule][2] = std::make_unique<TH1F>(Form("TrackletQ2_SM%d", supermodule), Form("TrackletQ2_SM%d", supermodule), 100, 0, 1000);
       getObjectsManager()->startPublishing(mTrackletQSupermodule[supermodule][2].get());
-    
     }
 
     mTrackletQSupermodule[supermodule][0]->Fill(trklt.getQ0());
