c2ce9e1f2cca2987550e231d297fc5fa5577c337
akavumpa <aswathy.menon@cern.ch>
2025-11-25 16:42:58 +0530

    cleaning!

--- Modules/TRD/runPbPb.sh
+++ Modules/TRD/runPbPb.sh
@@ -1,5 +1,5 @@
 configfile=$1
-o2-ctf-reader-workflow --ctf-input DatapPb.txt --onlyDet TRD |
+o2-ctf-reader-workflow --ctf-input DataPbPb.txt --onlyDet TRD |
 o2-qc -b --config json://"$configfile" &> output.log 
 
 
