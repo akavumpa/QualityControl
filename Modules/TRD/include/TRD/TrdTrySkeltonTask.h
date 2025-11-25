ec52522b8558c72430e0cf85c4d7f6a92b342f63
akavumpa <aswathy.menon@cern.ch>
2025-11-25 23:13:26 +0530

    Extra histos

--- Modules/TRD/include/TRD/TrdTrySkeltonTask.h
+++ Modules/TRD/include/TRD/TrdTrySkeltonTask.h
@@ -21,6 +21,7 @@
 #include <memory>
 
 class TH1F;
+class TH2F;
 
 using namespace o2::quality_control::core;
 
@@ -56,6 +57,10 @@ class TrdTrySkeltonTask final : public TaskInterface
   std::unique_ptr<TH1F> histChamber;   // tracklets per chamber
   std::unique_ptr<TH1F> histPadRow;    // tracklets per padrow
   std::unique_ptr<TH1F> histMCM;       // tracklets per MCM (non-zero)
+
+  std::unique_ptr<TH2F> histPadRowVsDet;
+  std::unique_ptr<TH1F> histMCMOccupancy;
+
 };
 
 } // namespace o2::quality_control_modules::trd
