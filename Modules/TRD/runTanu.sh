05311de8f1e654395849871d4be3ee340e5ebf23
akavumpa <aswathy.menon@cern.ch>
2026-01-29 00:19:52 +0530

    back to old start

--- Modules/TRD/runTanu.sh
+++ Modules/TRD/runTanu.sh
@@ -1,3 +1,3 @@
 configfile=$1
-o2-ctf-reader-workflow -b --ctf-input DataPbPb.txt --onlyDet TRD|
+o2-ctf-reader-workflow -b --ctf-input Data.txt --onlyDet TRD|
 o2-qc -b --config json://"$configfile" &> output.log
