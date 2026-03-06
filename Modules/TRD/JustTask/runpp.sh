b344e8b2caf49f65441c57ecb59ff79b864b1bd1
akavumpa <aswathy.menon@cern.ch>
2026-03-06 11:15:55 +0530

    just

--- Modules/TRD/JustTask/runpp.sh
+++ Modules/TRD/JustTask/runpp.sh
@@ -1,5 +1,5 @@
 configfile=$1
-o2-ctf-reader-workflow --ctf-input DatappALL.txt --onlyDet TRD |
+o2-ctf-reader-workflow --ctf-input Datapp.txt --onlyDet TRD |
 o2-qc -b --config json://"$configfile" &> outputpp.log 
 
 
