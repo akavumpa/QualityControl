841f172c231adad411940f46fbe11fcfb8884da2
akavumpa <aswathy.menon@cern.ch>
2026-03-06 15:43:00 +0530

    ..

--- Modules/TRD/runpp.sh
+++ Modules/TRD/runpp.sh
@@ -1,6 +1,6 @@
 configfile=$1
-o2-ctf-reader-workflow --ctf-input DatappALL.txt --onlyDet TRD |
-o2-qc -b --config json://"$configfile" &> outputppALL.log 
+o2-ctf-reader-workflow --ctf-input Datapp.txt --onlyDet TRD |
+o2-qc -b --config json://"$configfile" &> outputppSkeltonCheck.log 
 
 
 
